8fd19a0a10d96f4c5c523a4f960c2a1269a449e1
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2023-04-01 14:31:33 +0500

    new changes

---
+++ aapna_mart/lib/Screen/Splash.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            child: Image(image: AssetImage('assets/images/logo.png')),
+          ),
+        ],
+      ),
+    );
+  }
+}
